b85ddb65252f0292d6318dffd03a2995788482f7
Igor Zaplentnev <igor.zapletnev@gmail.com>
2016-11-02 11:35:22 +0700

    Initial projects

---
+++ PeriodicTable/UI/Main/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PeriodicTable
+//
+//  Created by Victor Sukochev on 02/11/2016.
+//  Copyright © 2016 Exyte. All rights reserved.
+//
+
+import Macaw
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
